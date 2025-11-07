da5776ebb5d6a3792da315b893e2f087d9c63832
Kauan Decarli <kauandecarli@gmail.com>
2025-11-07 08:11:43 -0300

    front: redirect to the correct page from home

--- front/app/routes/home.tsx
+++ front/app/routes/home.tsx
@@ -1,8 +1,20 @@
 import { HelloWorld } from "~/components/hello-world";
 import type { Route } from "./+types/home";
+import { getSession } from "~/utils/session.server";
+import { redirect } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Vocab Map" }];
+}
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+
+  if (!session.has("token")) {
+    return redirect("/login");
+  }
+
+  return redirect("/view");
 }
 
 export default function Home() {
